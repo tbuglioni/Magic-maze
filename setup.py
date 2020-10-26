54557aad80277442e553e5798c3173239cfacf29
tbuglioni <thomas.buglioni@gmail.com>
2020-10-26 11:26:25 +0100

    Clean file (delete what not's need to be keep)

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "projet 3 Buglioni Thomas",
-    version = "1",
-    description = "game with mcgyver",
-    executables = [Executable("main.py")]
-)
